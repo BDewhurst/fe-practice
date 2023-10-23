4569c6b0c057f52ab5836975f310e0cd9578c38b
Brad <bradleydewhurst123@outlook.com>
2023-10-23 14:51:26 +0100

    Url path added

--- takeaway/src/assets/components/RestaurantByCuisineCard.jsx
+++ takeaway/src/assets/components/RestaurantByCuisineCard.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { getRestaurantByCuisine } from "../../../api"
 import { AiFillStar } from "react-icons/ai"
+import { Link } from "react-router-dom"
 
 function RestaurantByCuisineCard () {
 
@@ -21,15 +22,20 @@ useEffect(() => {
         <main className="restaurant-container">
 
   <div className="restaurant-details">
+
     {restaurant.map(({ restaurant_id, name, address, cuisine, rating, article_img_url }) => (
+          <Link to={`/Restaurant/${restaurant_id}`} key={restaurant_id}>
       <div key={restaurant_id} className="restaurant-card">
+        
         <h1 className="restaurant-name">{name}</h1>
       <p>{address}</p>
       <p>{cuisine}</p>
         <p className="Rating-container"><AiFillStar/>{rating}</p>
-         <img src={article_img_url} alt={name} className="restaurant_img"/>  
-      </div>
+         <img src={article_img_url} alt={name} className="restaurant_img"/>
+          </div>
+      </Link>  
     ))}
+
   </div>
 </main>
 );
